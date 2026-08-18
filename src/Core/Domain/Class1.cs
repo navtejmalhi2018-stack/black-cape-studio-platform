2d81a3c1f9357d1dbab6499dfc8086d9a8bce8cb
Navtej Malhi <navtej.malhi2018@gmail.com>
2026-08-18 22:04:28 +1000

    Add Booking domain entity

--- src/Core/Domain/Class1.cs
+++ src/Core/Domain/Class1.cs
@@ -1,6 +1,20 @@
-﻿namespace Domain;
+﻿namespace Domain.Entities;
 
-public class Class1
+public class Booking
 {
+    public Guid Id { get; set; }
+    public Guid ClientId { get; set; }
+    public Guid ServiceId { get; set; }
+    public DateTime RequestedDate { get; set; }
+    public BookingStatus Status { get; set; }
+    public string? Notes { get; set; }
+}
 
+public enum BookingStatus
+{
+    Requested,
+    Confirmed,
+    InProgress,
+    Delivered,
+    Closed
 }
